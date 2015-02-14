91fccca07f3bcd2101053489b2ec506ee89013d2
yaboyanees <a.merzi@yahoo.com>
2015-02-13 19:24:09 -0600

    fixed homepage view issueby adding a check to see if employee is logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+  	@educations = Education.all
+  	@experiences = Experience.all
+  	@skills = Skill.all
+  	@trainings = Training.all
   end
 end
