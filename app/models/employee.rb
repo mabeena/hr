7b1ef3e6f267f8e2eba58b411ae72f65cae811cd
yaboyanees <a.merzi@yahoo.com>
2015-02-15 10:18:13 -0600

    added employee level and role select fields to the registeration form

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,10 @@ class Employee < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+	ROLE_TYPES = ["Supervisor", "Manager", "Team Member", "HR Manager"]
+	LEVEL_TYPES = ["A1-A3", "B1", "B2", "B3", "C1", "C2", "D", "E"]
+    
     has_many :evaluations
     has_many :experiences
     has_many :employee_trainings
