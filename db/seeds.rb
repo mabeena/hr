0455f3b889d1a8c57d7216e46b37279a9b750183
yaboyanees <a.merzi@yahoo.com>
2015-02-15 10:42:40 -0600

    started seeds file setup for support tables of our DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Education.create(:degree =>"bachelors", :concentration =>"computer science")
+
+#EDUCATION TABLE
+#high school
+Education.create(:degree =>"diploma/GED", :concentration =>"general studies")
+
+#undergrad
+Education.create(:degree =>"bachelor's", :concentration =>"computer science")
+#put 10 more major concentrations here 
+Education.create(:degree =>"bachelor's", :concentration =>"")
+Education.create(:degree =>"bachelor's", :concentration =>"other")
+
+#grad
+Education.create(:degree =>"master's", :concentration =>"computer science")
+#put 5 more concentrations here
+Education.create(:degree =>"master's", :concentration =>"")
+Education.create(:degree =>"master's", :concentration =>"other")
+
+#----------------------------------------------------
+#SKILL TABLE
+Skill.create(:name =>"java", :level=>"beginner")
+Skill.create(:name =>"java", :level=>"intermediate")
+Skill.create(:name =>"java", :level=>"advanced")
+
+Skill.create(:name =>"excel", :level=>"beginner")
 Skill.create(:name =>"excel", :level=>"intermediate")
+Skill.create(:name =>"excel", :level=>"advanced")
+#put 25 more skill sets here
+Skill.create(:name =>"", :level=>"")
+
+#----------------------------------------------------
+#TRAINING TABLE
 Training.create(:name =>"network+")
-Position.create(:name =>"insider sales coach")
+Training.create(:name =>"security+")
+Training.create(:name =>"salesforce")
+Training.create(:name =>"six sigma")
+Training.create(:name =>"PMP")
+#put 25 more training
+Training.create(:name =>"")
+
+#----------------------------------------------------
+#POSITION TABLE
+Position.create(:name =>"q/a tester")
+Position.create(:name =>"jr programmer")
+Position.create(:name =>"sr programmer")
+Position.create(:name =>"sales coach")
+Position.create(:name =>"system analyst")
+Position.create(:name =>"project manager")
+#put 25 more positions
+Position.create(:name =>"")
