09705072464f0706b097b1a59e1f3dc4fea2aea9
yaboyanees <a.merzi@yahoo.com>
2015-04-03 16:41:16 -0500

    update

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,5 +1,7 @@
 class PromotionsController < ApplicationController 
   def index
+  #@employee_educations = Education.joins(:employee_educations).select("degree")
+  @educations = Education.select("degree")
   end
   def show
   end
